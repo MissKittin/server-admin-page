7f4e2968bfabee714aab59e39d29e17cf03612f2
MissKittin <56042452+MissKittin@users.noreply.github.com>
2019-10-13 22:20:27 +0200

    Update router.php

--- www/router.php
+++ www/router.php
@@ -4,17 +4,19 @@
 	$system_location_php=$_SERVER['DOCUMENT_ROOT'] . ''; // for php scripts
 	$system_title='Router';
 
-	// router script
-	// *.sh and *.rc absolute denied
-	// readme texts also
-	// menu.html in address bar denied
-
-
-	/* Router script - filter */
+	/* This script:
+		- hides itself
+		- handles 404 file not found error
+		- deny access to *.sh, *.rc and *.txt files
+		- preventing access to module if index.php is in URI
+		- deny access to 'disabled' file
+		- deny access to disabled modules
+	*/
 
 	// hide this script - fake 404
-	if(strtok($_SERVER['REQUEST_URI'], '?') === '/router.php')
+	if(strtok($_SERVER['REQUEST_URI'], '?') === $system_location_html . '/router.php')
 	{
+		http_response_code(404);
 		echo '<!DOCTYPE html>
 			<html>
 				<head>
@@ -31,6 +33,7 @@
 	/*not file exist cut after $CHAR absolute path                           $CHAR */
 	if(!file_exists(strtok($_SERVER['DOCUMENT_ROOT'].$_SERVER['REQUEST_URI'], '?')))
 	{
+		http_response_code(404);
 		if(substr(strtok($_SERVER['REQUEST_URI'], '?'), -1) === '/')
 			$url='..';
 		else
@@ -51,11 +54,54 @@
 	// denied file types
 	if(preg_match('/\.(?:sh|rc|txt)$/', $_SERVER['REQUEST_URI'])) // if type ****.xxx in url
 	{
+		http_response_code(404);
 		echo '<html>
 			<head>
 				<title>'.$system_title.'</title>
 				'; include($system_location_php . '/lib/htmlheaders.php'); echo '
 				<meta http-equiv="refresh" content="0; url=.">
+			</head>
+		</html>';
+		exit();
+	}
+
+	// url with index.php destroys application - prevent this
+	if(strtok(substr($_SERVER['REQUEST_URI'], strrpos($_SERVER['REQUEST_URI'], '/') + 1), '?') === 'index.php')
+	{
+		http_response_code(404);
+		echo '<html>
+			<head>
+				<title>'.$system_title.'</title>
+				'; include($system_location_php . '/lib/htmlheaders.php'); echo '
+				<meta http-equiv="refresh" content="0; url=.">
+			</head>
+		</html>';
+		exit();
+	}
+
+	// fake 404 if path point to file 'disabled'
+	if(strtok(substr($_SERVER['REQUEST_URI'], strrpos($_SERVER['REQUEST_URI'], '/') + 1), '?') === 'disabled')
+	{
+		http_response_code(404);
+		echo '<html>
+			<head>
+				<title>'.$system_title.'</title>
+				'; include($system_location_php . '/lib/htmlheaders.php'); echo '
+				<meta http-equiv="refresh" content="0; url=..">
+			</head>
+		</html>';
+		exit();
+	}
+
+	// deny access to disabled modules
+	if(file_exists($system_location_php . strtok($_SERVER['REQUEST_URI'], '?') . '/disabled'))
+	{
+		http_response_code(404);
+		echo '<html>
+			<head>
+				<title>'.$system_title.'</title>
+				'; include($system_location_php . '/lib/htmlheaders.php'); echo '
+				<meta http-equiv="refresh" content="0; url=..">
 			</head>
 		</html>';
 		exit();
